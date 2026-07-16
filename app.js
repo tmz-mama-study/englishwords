9461a62075a4674cb19ae103d2bdb931f3fd1470
tmz-mama-study <tmz.24555+wk@gmail.com>
2026-07-16 17:14:44 +0900

    Refactor app.js for improved test functionality

--- app.js
+++ app.js
@@ -5,9 +5,22 @@ const wordCount = document.getElementById("wordCount");
 const answerInput = document.getElementById("answerInput");
 const resultArea = document.getElementById("resultArea");
 const checkAnswerBtn = document.getElementById("checkAnswerBtn");
+const keyboard = document.getElementById("keyboard");
 
 let words = [];
+let questionOrder = [];
+let currentPosition = 0;
+let currentWord = null;
 
+let correctCount = 0;
+let wrongCount = 0;
+let weakWords = [];
+
+let modewk=-1;
+const mode_read=0;
+const mode_spel=1;
+
+shareBtn.hidden = true;
 window.addEventListener("DOMContentLoaded", loadBooks);
 
 async function loadBooks() {
@@ -108,14 +121,10 @@ function enableButtons() {
 }
 
 
-let questionOrder = [];
-let currentPosition = 0;
-let currentWord = null;
-
 // ***************************************************************
 // 問題表示
 // ***************************************************************
-function showQuestion() {
+function showQuestion(mode) {
 
     if (currentPosition >= questionOrder.length) {
         finishTest();
@@ -123,15 +132,33 @@ function showQuestion() {
     }
 
     currentWord = questionOrder[currentPosition];
-
-    questionWord.textContent = currentWord.english;
+	
+	let questStr = "";
+	let inputStr = "";
+	
+	switch(mode){
+	case mode_read:
+		questStr = currentWord.english;
+		answerInput.disabled = false;
+		inputStr="日本語を入力";
+		break;
+	case mode_spel:
+		questStr = currentWord.japanese;
+		answerInput.disabled = true;
+		break;
+	}
+	questionWord.textContent ="("+ (currentPosition+1) + ") " +questStr;
 
     answerInput.value = "";
-    resultArea.textContent = "";
-
+	resultArea.textContent = "";
+	
+	const inpt = document.querySelectorAll('#answerInput')[0];
+	inpt.placeholder = inputStr;
+	
 	checkAnswerBtn.disabled = false;
 	nextQuestionBtn.hidden = true;
 }
+
 // 問題シャッフル
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -142,56 +169,271 @@ function shuffle(array) {
 }
 
 // ***************************************************************
+// テスト開始 共通関数
+// ***************************************************************
+function startTest(){
+	const targetElement = event.target;
+
+	quizFinish.style.display = "none";
+	readTest.style.display = "none";
+	spelTest.style.display = "none";
+
+	shareBtn.hidden = true;
+
+	quiztitle.textContent = targetElement.textContent;
+
+	questionOrder = [...words];
+    shuffle(questionOrder);
+
+	currentPosition = 0;
+	correctCount = 0;
+	wrongCount = 0;
+	weakWords = [];
+}
+
+
+// ***************************************************************
 // 読みテスト
 // ***************************************************************
 document.getElementById("readingBtn").addEventListener("click", startReadingTest);
 function startReadingTest() {
 	
-	const targetElement = event.target;
-    quiztitle.textContent = targetElement.textContent;
+	startTest();
+	modewk = mode_read;
 	
-    questionOrder = [...words];
-    shuffle(questionOrder);
-
-    currentPosition = 0;
     quizArea.style.display = "block";
-    showQuestion();
+    readTest.style.display = "block";
+
+    showQuestion(modewk);
 }
+
+
+
+
+
+// ***************************************************************
+// スペルテスト
+// ***************************************************************
+document.getElementById("spellingBtn").addEventListener("click", startSpellingTest);
+function startSpellingTest() {
+	
+	startTest();
+	modewk = mode_spel;
+
+	createKeyboard();
+
+	quizArea.style.display = "block";
+    spelTest.style.display = "block";
+
+    showQuestion(modewk);
+}
+
+
+
+function showSpellingQuestion() {
+
+    currentWord = questionOrder[currentPosition];
+    spellingQuestion.textContent = currentWord.japanese;
+
+    spellingResult.textContent = "";
+    nextSpellingBtn.disabled = true;
+}
+function createKeyboard() {
+
+    const rows = [
+        "QWERTYUIOP",
+        "ASDFGHJKL",
+        "ZXCVBNM"
+    ];
+
+    keyboard.innerHTML = "";
+
+    rows.forEach(row => {
+
+        const rowDiv = document.createElement("div");
+        rowDiv.className = "keyboard-row";
+
+        row.split("").forEach(letter => {
+
+            const button = document.createElement("button");
+
+            button.textContent = letter;
+
+            button.addEventListener("click", () => {
+                answerInput.value += letter.toLowerCase();
+            });
+
+            rowDiv.appendChild(button);
+
+        });
+
+        keyboard.appendChild(rowDiv);
+
+    });
+}
+// ***************************************************************
+// キーボードクリア
+// ***************************************************************
+clearBtn.addEventListener("click", () => {
+	answerInput.value = "";
+});
+// ***************************************************************
+// キーボードBackSpace
+//// ***************************************************************
+backspaceBtn.addEventListener("click", () => {
+    answerInput.value = answerInput.value.slice(0, -1);
+});
+
+
+
+
+
+
 
 // ***************************************************************
 // 回答表示
 // ***************************************************************
 checkAnswerBtn.addEventListener("click", checkAnswer);
 function checkAnswer() {
-    const answer = answerInput.value.trim();
+	const answer = answerInput.value.trim();
 
-    if (answer === currentWord.japanese) {
-        resultArea.textContent = "○ 正解";
-    } else {
-        resultArea.textContent =
-            `× 不正解　　答え：${currentWord.japanese}`;
-    }
+	let str="";
+	switch(modewk){
+	case mode_read:
+		str = currentWord.japanese;
+		break;
+	case mode_spel:
+		str = currentWord.english;
+		break;
+	}
+	
+	
+	if (answer === str) {
+		resultArea.textContent = "○ 正解";
+		correctCount++;
+	} else {
+		resultArea.textContent = `× 不正解　　答え：${str}`;
+
+		wrongCount++;
+		weakWords.push(currentWord)
+		addWeakWord(currentWord);
+	}
+
     checkAnswerBtn.disabled = true;
+	answerInput.disabled = true;
+	
 	nextQuestionBtn.hidden = false;
 }
-
 // ***************************************************************
 // 次の問題
 // ***************************************************************
 nextQuestionBtn.addEventListener("click", () => {
     currentPosition++;
-    showQuestion();
+    showQuestion(modewk);
 });
 
 // ***************************************************************
 // 終了
 // ***************************************************************
 function finishTest() {
-    quizArea.innerHTML = `
+    const total = correctCount + wrongCount;
+
+    const rate =
+        total === 0
+            ? 0
+            : Math.round(correctCount / total * 100);
+
+    let weakWordHtml = "";
+
+    if (weakWords.length > 0) {
+        weakWordHtml = `
+            <h3>苦手単語</h3>
+            <ul>
+                ${weakWords
+                    .map(word =>
+                        `<li>${word.english} : ${word.japanese}</li>`)
+                    .join("")}
+            </ul>
+        `;
+    }
+
+	shareBtn.hidden = false;
+    quizArea.style.display = "none";
+    quizFinish.style.display = "block";
+
+    quizFinish.innerHTML = `
         <h2>テスト終了</h2>
-        <p>${questionOrder.length}問が終了しました。</p>
+        <p>全${total}問</p>
+        <p>正解： ${correctCount}問／不正解 ${wrongCount}問</p>
+        <p>正答率 ${rate}%</p>
+        ${weakWordHtml}
     `;
 }
 
 
 
+// ***************************************************************
+// 苦手単語の読込
+// ***************************************************************
+function loadWeakWords() {
+    const saved =
+        localStorage.getItem("weakWords");
+
+    if (saved) {
+        weakWords = JSON.parse(saved);
+    }
+}
+// ***************************************************************
+// 苦手単語の登録
+// ***************************************************************
+function addWeakWord(word) {
+
+    const exists = weakWords.some(
+        item => item.english === word.english
+    );
+
+    if (!exists) {
+        weakWords.push(word);
+
+//        localStorage.setItem(
+//            "weakWords",
+//            JSON.stringify(weakWords)
+//        );
+		console.log(weakWords);
+    }
+}
+
+
+// ***************************************************************
+// 共有
+// ***************************************************************
+document.getElementById("shareBtn").addEventListener("click", shareResult);
+function shareResult() {
+
+	let testModeStr = "";
+	
+	switch(modewk){
+	case mode_read:
+		testModeStr = "読み";
+		break;
+	case mode_spel:
+		testModeStr = "スペル";
+		break;
+	}
+
+	const text =
+		`📚 英単語テスト結果(${testModeStr})
+
+正解 ${correctCount}問
+不正解 ${wrongCount}問`;
+
+    if (navigator.share) {
+        navigator.share({
+            title: "英単語テスト結果",
+            text: text
+        });
+    } else {
+        navigator.clipboard.writeText(text);
+        alert("結果をクリップボードにコピーしました");
+    }
+}
